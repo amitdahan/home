d9eb5199a56a5afac414d4c5d8932c9c22c8c2a7
Amit Dahan <amitda@wix.com>
2021-06-05 17:18:29 +0300

    Remove CHAT_ID

--- packages/telegram-bot/src/telegram.ts
+++ packages/telegram-bot/src/telegram.ts
@@ -1,16 +1,12 @@
 import { Telegraf } from "telegraf";
 
-const { TELEGRAM_TOKEN, TELEGRAM_CHAT_ID } = process.env;
+const { TELEGRAM_TOKEN } = process.env;
 
 export interface TelegramOptions {}
 
 export function setupTelegram({}: TelegramOptions = {}) {
   if (!TELEGRAM_TOKEN) {
     throw new Error("No TELEGRAM_TOKEN provided");
-  }
-
-  if (!TELEGRAM_CHAT_ID) {
-    throw new Error("No TELEGRAM_CHAT_ID provided");
   }
 
   const telegram = new Telegraf(TELEGRAM_TOKEN);
